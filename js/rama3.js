93d81208d6b4b0b2cfd23a85009dd311184d87a1
delatorrerita <125808988+delatorrerita@users.noreply.github.com>
2023-03-18 21:12:46 +0100

    Update rama3.js

--- js/rama3.js
+++ js/rama3.js
@@ -2,9 +2,9 @@
 const integrantes = [
 new Familiar('53', 1, 'f', 'c', 'Raúl Orlando', 'Chirivella Varela', 'm', '09-julio', 'm', false, '-'),
 new Familiar('53a', 1, 'p', 'c', 'Betty', 'Palencia', 'f', '', 'm', false, '-'),
-new Familiar('531', 2, 'f', 'd', 'Raúl Gerardo', 'Chirivella Palencia', 'm', '30-diciembre', '11-febrero', true, '-'),
-new Familiar('531a', 2, 'p', 'd', 'Maryllen', 'Tirado Nuñez', 'f', '30-noviembre', 'v', '-'),
-new Familiar('5311', 3, 'f', 'c', 'Raúl Gerardo', 'Chirivella Tirado', 'm', '8-enero', 'v', '-'),
+new Familiar('531', 2, 'f', 'd', 'Raúl Gerardo', 'Chirivella Palencia', 'm', '30-diciembre', '11-febrero', 'm', true, '-'),
+new Familiar('531a', 2, 'p', 'd', 'Maryllen', 'Tirado Nuñez', 'f', '30-noviembre', '', 'v', '-'),
+new Familiar('5311', 3, 'f', 'c', 'Raúl Gerardo', 'Chirivella Tirado', 'm', '8-enero', '', 'v', '-'),
 new Familiar('5311a', 3, 'p', 'c', 'Andrea', 'González Paniz', 'f', '', '', 'v', '-'),
 new Familiar('5312', 3, 'f', 's', 'Juan Pablo', 'Chirivella Tirado', 'm', '', '', 'v', '-'),
 new Familiar('532', 2, 'f', 's', 'Orlando Rafael', 'Chirivella Palencia', 'm', '12-mayo', '19-julio', false, '-'),
