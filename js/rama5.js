fb217825e1018cd4cd38f6e49f3055a3f1d3f1c8
delatorrerita <125808988+delatorrerita@users.noreply.github.com>
2023-03-18 18:30:30 +0100

    Update rama5.js

--- js/rama5.js
+++ js/rama5.js
@@ -2,9 +2,9 @@
 const integrantes = [
 new Familiar('55', 1, 'f', 'c', 'Ligia Josefina', 'Chirivella Varela', 'f', '27-agosto', '14-diciembre', false, '-'),
 new Familiar('55a', 1, 'p', 'v', 'Víctor Manuel', 'de la Torre Rincón', 'm', '22-agosto', '', false, '-'),
-new Familiar('551', 2, 'f', 'b', 'Rita Sofía', 'de la Torre Chirivella', 'f', '02-julio', '', true, 'https://delatorrerita.es/'),
+new Familiar('551', 2, 'f', 'b', 'Rita Sofía', 'de la Torre Chirivella', 'f', '02-julio', '', true, 'https://delatorrerita.github.io/perfiles/rita.html'),
 new Familiar('551a', 2, 'p', 'd', 'José Antonio', 'Lanau', 'm', '05-enero', '', false, '-'),
-new Familiar('5511', 3, 'f', 'b', 'Laura Sofía', 'Lanau de la Torre', 'f', '03-febrero', '', false, 'https://danta.es/'),
+new Familiar('5511', 3, 'f', 'b', 'Laura Sofía', 'Lanau de la Torre', 'f', '03-febrero', '', false, 'https://delatorrerita.github.io/perfiles/laura.html'),
 new Familiar('552', 2, 'f', 'b', 'Víctor Manuel', 'de la Torre Chirivella', 'm', '11-octubre', '', true, '-'),
 new Familiar('552a', 2, 'p', 'c', 'Esther', 'García', 'f', '28-diciembre', '', false, '-'),
 new Familiar('553', 2, 'f', 'b', 'Myriam Josefina', 'de la Torre Chirivella', 'f', '22-noviembre', '', true, '-'),
