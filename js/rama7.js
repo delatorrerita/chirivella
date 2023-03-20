a08419edf091ae0415b316a03ebae0aa1afcc551
delatorrerita <125808988+delatorrerita@users.noreply.github.com>
2023-03-20 17:50:01 +0100

    Update rama7.js

--- js/rama7.js
+++ js/rama7.js
@@ -17,11 +17,11 @@ new Familiar('572b', 2, 'p', 'd', 'Sagrario', 'Pérez', 'f', '', '', false, '-')
 new Familiar('5723', 3, 'f', 's', 'Arturo Manuel', 'Chirivella Pérez', 'm', '', 'v', false, '-'),
 
 new Familiar('573', 2, 'f', 'c', 'María de los Ángeles', 'Chirivella Landaeta', 'f', '16-marzo', 'v', true, '-'),
-new Familiar('573a', 2, 'p', 'd', 'José', 'Abraham', 'm', '', 'v', false, '-'),
+new Familiar('573a', 2, 'p', 'd', 'José', 'Abraham', 'm', '13-noviembre', 'v', false, '-'),
 new Familiar('5731', 3, 'f', 's', 'José Rafael', 'Abraham Chirivella', 'm', '04-enero', 'v', false, '-'),
 new Familiar('573b', 2, 'p', 'c', 'Emilio', 'Cartelle', 'm', '23-diciembre', 'v', false, '-'),
-new Familiar('5732', 3, 'f', 's', 'Ángel Manuel', 'Cartelle Chirivella', 'm', '05-diciembre', 'v', false, '-'),
-new Familiar('5733', 3, 'f', 's', 'Emilio', 'Cartelle Chirivella', 'm', '04-octubre', 'v', false, '-'),
+new Familiar('5732', 3, 'f', 's', 'Ángel Manuel', 'Cartelle Chirivella', 'm', '10-diciembre', 'v', false, '-'),
+new Familiar('5733', 3, 'f', 's', 'Emilio', 'Cartelle Chirivella', 'm', '03-octubre', 'v', false, '-'),
 
 new Familiar('574', 2, 'f', 'r', 'Rudith Angélica', 'Chirivella Landaeta', 'f', '28-febrero', 'v', true, '-'),
 new Familiar('574a', 2, 'p', 'r', 'José Manuel', 'Pacheco', 'm', '', 'v', false, '-'),
