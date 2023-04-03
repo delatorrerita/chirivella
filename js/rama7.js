76e61c47b0e273f4a0f16d18184d9a4147181f0c
delatorrerita <125808988+delatorrerita@users.noreply.github.com>
2023-04-03 13:50:26 +0200

    Update rama7.js

--- js/rama7.js
+++ js/rama7.js
@@ -1,30 +1,31 @@
-//constructor(id, nivel, tipo, status, nombres, apellidos, sexo, nace, muere, detalle, url)
+//constructor(id, nivel, tipo, status, nombres, apellidos, sexo, nace, muere, detalle, url, foto)
 const integrantes = [
-new Familiar('57', 1, 'f', 'd', 'Avilio Alfonso', 'Chirivella Varela', 'm', '22-febrero', '23-junio', false, 'https://enyoranzas.blogspot.com/'),
-new Familiar('57a', 1, 'p', 'd', 'Judith Marina', "Landaeta D'Aubeterre", 'f', '24-abril', 'v', false, '-'),
+  new Familiar('57', 1, 'f', 'd', 'Avilio Alfonso', 'Chirivella Varela', 'm', '22-febrero', '23-junio', false, 'https://enyoranzas.blogspot.com/', true),
+  new Familiar('57a', 1, 'p', 'd', 'Judith Marina', "Landaeta D'Aubeterre", 'f', '24-abril', 'v', false, '-', true),
+  
+  new Familiar('571', 2, 'f', 'c', 'Oxmerari Judith', 'Chirivella Landaeta', 'f', '27-febrero', 'v', true, '@dra.chirivella', true),
+  new Familiar('571a', 2, 'p', 'c', 'Jorge Eduardo', 'Tovar García', 'm', '13-noviembre', 'v', false, '-', true),
+  new Familiar('5711', 3, 'f', 's', 'Oxmerari Angely', 'Tovar Chirivella', 'f', '01-agosto', 'v', false, '-', true),
+  new Familiar('5712', 3, 'f', 's', 'Isarith Alexandra', 'Tovar Chirivella', 'f', '18-marzo', 'v', false, '-', true),
+  
+  new Familiar('572', 2, 'f', 'd', 'Avilio Arturo Manuel', 'Chirivella Landaeta', 'm', '31-octubre', 'v', true, '-', true),
+  new Familiar('572a', 2, 'p', 'd', 'Arely Yanet', 'Montes de Oca', 'f', '12-noviembre', 'v', false, '-', true),
+  new Familiar('5721', 3, 'f', 's', 'Arturo Alejandro', 'Chirivella Montes de Oca', 'm', '16-octubre', 'v', false, '-', true),
+  new Familiar('5722', 3, 'f', 'c', 'Ariadna Carolina', 'Chirivella Montes de Oca', 'f', '17-noviembre', 'v', false, '-', true),
+  new Familiar('5722a', 3, 'p', 'c', 'José Gregorio', 'Petit Goitía', 'f', '20-abril', '', false, '-', true),  
+  new Familiar('57221', 4, 'f', 's', 'Matías Lorenzo', 'Petit Chirivella', 'm', '25-agosto', 'v', false, '-', true),
+  new Familiar('572b', 2, 'p', 'd', 'Sagrario de Jesús', 'Pérez Tejeda', 'f', '9-noviembre', '', false, '-', true),
+  new Familiar('5723', 3, 'f', 's', 'Arturo Manuel', 'Chirivella Pérez', 'm', '25-mayo', 'v', false, '-', true),
+  new Familiar('572c', 2, 'p', 'c', 'Gloria', 'Manzano Puebla', 'f', '31-marzo', '', false, '-', true),
+  
+  new Familiar('573', 2, 'f', 'c', 'María de los Ángeles', 'Chirivella Landaeta', 'f', '16-marzo', 'v', true, '-', true),
+  new Familiar('573a', 2, 'p', 'd', 'José Rafael', 'Abraham Méndez', 'm', '13-noviembre', 'v', false, '-', true),
+  new Familiar('5731', 3, 'f', 's', 'José Rafael', 'Abraham Chirivella', 'm', '04-enero', 'v', false, '-', true),
+  new Familiar('573b', 2, 'p', 'c', 'Emilio Manuel', 'Cartelle Rodríguez', 'm', '23-diciembre', 'v', false, '-', true),
+  new Familiar('5732', 3, 'f', 's', 'Ángel Manuel', 'Cartelle Chirivella', 'm', '10-diciembre', 'v', false, '-', true),
+  new Familiar('5733', 3, 'f', 's', 'Emilio Manuel', 'Cartelle Chirivella', 'm', '03-octubre', 'v', false, '-', true),
+  
+  new Familiar('574', 2, 'f', 'c', 'Rudith Angélica', 'Chirivella Landaeta', 'f', '28-febrero', 'v', true, '-', true),
+  new Familiar('574a', 2, 'p', 'c', 'José Manuel', 'Pacheco Castellanos', 'm', '8-febrero', 'v', false, '-', true),
+  ]
 
-new Familiar('571', 2, 'f', 'c', 'Oxmerari Judith', 'Chirivella Landaeta', 'f', '27-febrero', 'v', true, '-'),
-new Familiar('571a', 2, 'p', 'c', 'Jorge Eduardo', 'Tovar García', 'm', '13-noviembre', 'v', false, '-'),
-new Familiar('5711', 3, 'f', 's', 'Oxmerari Angely', 'Tovar Chirivella', 'f', '01-agosto', 'v', false, '-'),
-new Familiar('5712', 3, 'f', 's', 'Isarith Alexandra', 'Tovar Chirivella', 'f', '18-marzo', 'v', false, '-'),
-
-new Familiar('572', 2, 'f', 'd', 'Avilio Arturo Manuel', 'Chirivella Landaeta', 'm', '31-octubre', 'v', true, '-'),
-new Familiar('572a', 2, 'p', 'd', 'Arely Yanet', 'Montes de Oca', 'f', '12-noviembre', 'v', false, '-'),
-new Familiar('5721', 3, 'f', 's', 'Arturo Alejandro', 'Chirivella Montes de Oca', 'm', '16-octubre', 'v', false, '-'),
-new Familiar('5722', 3, 'f', 'c', 'Ariadna Carolina', 'Chirivella Montes de Oca', 'f', '17-noviembre', 'v', false, '-'),
-new Familiar('5722a', 3, 'p', 'c', 'José Gregorio', 'Petit Goitía', 'f', '20-abril', '', false, '-'),  
-new Familiar('57221', 4, 'f', 's', 'Matías Lorenzo', 'Petit Chirivella', 'm', '25-agosto', 'v', false, '-'),
-new Familiar('572b', 2, 'p', 'd', 'Sagrario de Jesús', 'Pérez Tejeda', 'f', '9-noviembre', '', false, '-'),
-new Familiar('5723', 3, 'f', 's', 'Arturo Manuel', 'Chirivella Pérez', 'm', '25-mayo', 'v', false, '-'),
-new Familiar('572c', 2, 'p', 'c', 'Gloria', 'Manzano Puebla', 'f', '31-marzo', '', false, '-'),
-
-new Familiar('573', 2, 'f', 'c', 'María de los Ángeles', 'Chirivella Landaeta', 'f', '16-marzo', 'v', true, '-'),
-new Familiar('573a', 2, 'p', 'd', 'José Rafael', 'Abraham Méndez', 'm', '13-noviembre', 'v', false, '-'),
-new Familiar('5731', 3, 'f', 's', 'José Rafael', 'Abraham Chirivella', 'm', '04-enero', 'v', false, '-'),
-new Familiar('573b', 2, 'p', 'c', 'Emilio Manuel', 'Cartelle Rodríguez', 'm', '23-diciembre', 'v', false, '-'),
-new Familiar('5732', 3, 'f', 's', 'Ángel Manuel', 'Cartelle Chirivella', 'm', '10-diciembre', 'v', false, '-'),
-new Familiar('5733', 3, 'f', 's', 'Emilio Manuel', 'Cartelle Chirivella', 'm', '03-octubre', 'v', false, '-'),
-
-new Familiar('574', 2, 'f', 'c', 'Rudith Angélica', 'Chirivella Landaeta', 'f', '28-febrero', 'v', true, '-'),
-new Familiar('574a', 2, 'p', 'c', 'José Manuel', 'Pacheco Castellanos', 'm', '8-febrero', 'v', false, '-'),
-]
