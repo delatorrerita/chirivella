d02a07c52da25d1a97f29e216cde666757c492e1
delatorrerita <125808988+delatorrerita@users.noreply.github.com>
2023-04-03 13:41:57 +0200

    Update rama3.js

--- js/rama3.js
+++ js/rama3.js
@@ -1,11 +1,11 @@
-//constructor(id, nivel, tipo, status, nombres, apellidos, sexo, nace, muere, detalle, url)
+//constructor(id, nivel, tipo, status, nombres, apellidos, sexo, nace, muere, detalle, url, foto)
 const integrantes = [
-new Familiar('53', 1, 'f', 'c', 'Raúl Orlando', 'Chirivella Varela', 'm', '09-julio', 'm', false, '-'),
-new Familiar('53a', 1, 'p', 'c', 'Betty', 'Palencia', 'f', '', 'm', false, '-'),
-new Familiar('531', 2, 'f', 'd', 'Raúl Gerardo', 'Chirivella Palencia', 'm', '30-diciembre', '11-febrero', true, '-'),
-new Familiar('531a', 2, 'p', 'd', 'Maryllen', 'Tirado', 'f', '30-noviembre', '', 'v', '-'),
-new Familiar('5311', 3, 'f', 'c', 'Raúl Gerardo', 'Chirivella Tirado', 'm', '08-enero', '', 'v', '-'),
-new Familiar('5311a', 3, 'p', 'c', 'Andrea', 'González Paniz', 'f', '', '', 'v', '-'),
-new Familiar('5312', 3, 'f', 's', 'Juan Pablo', 'Chirivella Tirado', 'm', '', '', 'v', '-'),
-new Familiar('532', 2, 'f', 's', 'Orlando Rafael', 'Chirivella Palencia', 'm', '12-mayo', '19-julio', false, '-'),
+new Familiar('53', 1, 'f', 'c', 'Raúl Orlando', 'Chirivella Varela', 'm', '09-julio', 'm', false, '-', true),
+new Familiar('53a', 1, 'p', 'c', 'Betty', 'Palencia', 'f', '', 'm', false, '-', true),
+new Familiar('531', 2, 'f', 'd', 'Raúl Gerardo', 'Chirivella Palencia', 'm', '30-diciembre', '11-febrero', true, '-', true),
+new Familiar('531a', 2, 'p', 'd', 'Maryllen', 'Tirado', 'f', '30-noviembre', '', 'v', '-', true),
+new Familiar('5311', 3, 'f', 'c', 'Raúl Gerardo', 'Chirivella Tirado', 'm', '08-enero', '', 'v', '-', true),
+new Familiar('5311a', 3, 'p', 'c', 'Andrea', 'González Paniz', 'f', '', '', 'v', '-', true),
+new Familiar('5312', 3, 'f', 's', 'Juan Pablo', 'Chirivella Tirado', 'm', '', '', 'v', '-', true),
+new Familiar('532', 2, 'f', 's', 'Orlando Rafael', 'Chirivella Palencia', 'm', '12-mayo', '19-julio', false, '-', true),
 ]
