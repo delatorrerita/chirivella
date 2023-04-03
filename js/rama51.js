8d42bb3d1e3b51d2b297ed5c12b0ecbe98bb810e
delatorrerita <125808988+delatorrerita@users.noreply.github.com>
2023-04-03 14:05:37 +0200

    Add files via upload

---
+++ js/rama51.js
@@ -0,0 +1,21 @@
+//constructor(id, nivel, tipo, status, nombres, apellidos, sexo, nace, muere, detalle, url, foto)
+const integrantes = [
+  new Familiar('51', 1, 'f', 'c', 'Armando Antonio', 'Chirivella Varela', 'm', '17-septiembre', '01-julio', false, '-', true),
+  new Familiar('51a', 1, 'p', 'c', 'Cecilia', 'Arellano', 'f', '03-noviembre', '19-abril', false, '-', true),
+  
+  new Familiar('511', 2, 'f', 'c', 'Darío Armando', 'Chirivella Arellano', 'm', '25-febrero', '11-abril', true, '-', false),
+  new Familiar('511a', 2, 'p', 'v', 'Belkys', 'Sánchez', 'f', '', 'v', false, '-', true),
+  new Familiar('5111', 3, 'f', 's', 'Darío Armando', 'Chirivella Sánchez', 'm', '', 'v', false, '-', true),
+  new Familiar('5112', 3, 'f', 'c', 'Darybel', 'Chirivella Sánchez', 'f', '', 'v', false, '-', true),
+  new Familiar('5113', 3, 'f', 's', 'Daniel Alejandro', 'Chirivella Sánchez', 'm', '', 'v', false, '-', true),
+  new Familiar('5114', 3, 'f', 's', 'David', 'Chirivella Sánchez', 'm', '', 'm', false, '-', true),
+  new Familiar('5115', 3, 'f', 's', 'Damián', 'Chirivella Sánchez', 'm', '', 'v', false, '-', false),
+  new Familiar('5116', 3, 'f', 's', 'Maricé', 'Chirivella Sánchez', 'f', '', 'v', false, '-', true),
+  
+  new Familiar('512', 2, 'f', 'c', 'Aída Cecilia', 'Chirivella Arellano', 'f', '10-septiembre', 'v', true, '-', true),
+  new Familiar('512a', 2, 'p', 'c', 'Ramón', 'Delgado', 'm', '', 'v', false, '-', true),
+  new Familiar('5121', 3, 'f', 's', 'Ricardo José', 'Delgado Chirivella', 'm', '', 'v', false, '-', true),
+  
+  new Familiar('513', 2, 'f', 's', 'César Antonio', 'Chirivella Arellano', 'm', '21-febrero', '31-octubre', false, '-', false),
+  new Familiar('514', 2, 'f', 's', 'Betty Lucía', 'Chirivella Arellano', 'f', '16-junio', 'v', false, '-', true),
+  ]
---
+++ js/rama55.js
@@ -0,0 +1,15 @@
+//constructor(id, nivel, tipo, status, nombres, apellidos, sexo, nace, muere, detalle, url, foto)
+const integrantes = [
+  new Familiar('55', 1, 'f', 'c', 'Ligia Josefina', 'Chirivella Varela', 'f', '27-agosto', '14-diciembre', false, '-', true),
+  new Familiar('55a', 1, 'p', 'v', 'Víctor Manuel', 'de la Torre Rincón', 'm', '22-agosto', '', false, '-', true),
+  new Familiar('551', 2, 'f', 'b', 'Rita Sofía', 'de la Torre Chirivella', 'f', '02-julio', '', true, 'https://delatorrerita.github.io/perfiles/rita.html', true),
+  new Familiar('551a', 2, 'p', 'd', 'José Antonio', 'Lanau', 'm', '05-enero', '', false, '-', true),
+  new Familiar('5511', 3, 'f', 'b', 'Laura Sofía', 'Lanau de la Torre', 'f', '03-febrero', '', false, 'https://delatorrerita.github.io/perfiles/laura.html', true),
+  new Familiar('552', 2, 'f', 'b', 'Víctor Manuel', 'de la Torre Chirivella', 'm', '11-octubre', '', true, '-', true),
+  new Familiar('552a', 2, 'p', 'c', 'Esther', 'García', 'f', '28-diciembre', '', false, '-', true),
+  new Familiar('553', 2, 'f', 'b', 'Myriam Josefina', 'de la Torre Chirivella', 'f', '22-noviembre', '', true, 'https://www.google.com/maps/place/Angeles+de+Jes%C3%BAs/@10.1880914,-68.0437603,13.2z/data=!4m15!1m8!3m7!1s0x8e80677e1282815d:0x17e7eeee6361730a!2sAngeles+de+Jes%C3%BAs!8m2!3d10.1977322!4d-68.0052719!10e4!16s%2Fg%2F1q64jdw67!3m5!1s0x8e80677e1282815d:0x17e7eeee6361730a!8m2!3d10.1977322!4d-68.0052719!16s%2Fg%2F1q64jdw67', true),
+  new Familiar('553a', 2, 'p', 'c', 'Raúl Antonio', 'Montoya', 'm', '31-octubre', '', false, '-', true),
+  new Familiar('5531', 3, 'f', 'b', 'Carolina', 'Montoya de la Torre', 'f', '03-febrero', '', false, '-', true),
+  new Familiar('5532', 3, 'f', 'b', 'Ligia Elena', 'Montoya de la Torre', 'f', '12-marzo', '', false, '-', true),
+  new Familiar('5533', 3, 'f', 'b', 'Raúl Alberto', 'Montoya de la Torre', 'm', '21-diciembre', '', false, '-', true),               
+  ]
