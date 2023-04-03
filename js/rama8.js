8c53a25a788669f53c72c7926cc196486426b40c
delatorrerita <125808988+delatorrerita@users.noreply.github.com>
2023-04-03 13:52:47 +0200

    Update rama8.js

--- js/rama8.js
+++ js/rama8.js
@@ -1,15 +1,15 @@
-//constructor(id, nivel, tipo, status, nombres, apellidos, sexo, nace, muere, detalle, url)
+//constructor(id, nivel, tipo, status, nombres, apellidos, sexo, nace, muere, detalle, url, foto)
 const integrantes = [
-new Familiar('58', 1, 'f', 'c', 'Carmen Judith', 'Chirivella Varela', 'f', '16-julio', '29-agosto', false, '-'),
-new Familiar('58a', 1, 'p', 'c', 'Marco Antonio', 'Rodríguez', 'm', '07-octubre', 'v', false, '-'),
-new Familiar('581', 2, 'f', 'c', 'Ana Rita', 'Rodríguez Chirivella', 'f', '16-septiembre', 'v', true, '-'),
-new Familiar('581a', 2, 'p', 'c', 'Abelardo Rafael', 'Valentíner Chirivella', 'm', '30-septiembre', 'v', false, '-'),
-new Familiar('5811', 3, 'f', 'c', 'Abelardo Antonio', 'Valentíner Rodríguez', 'm', '10-noviembre', 'v', false, '-'),
-new Familiar('5811a', 3, 'p', 'c', 'Mariangeles', 'López', 'f', '', 'v', false, '-'),
-new Familiar('582', 2, 'f', 'c', 'Carmen Cecilia', 'Rodríguez Chirivella', 'f', '16-mayo', 'v', true, '-'),
-new Familiar('582a', 2, 'p', 'c', 'Omar Sergio', 'Gugliesi', 'm', '', 'v', false, '-'),
-new Familiar('5821', 3, 'f', 'c', 'Sergio Andrés', 'Gugliesi Rodríguez', 'm', '02-octubre', 'v', false, '-'),
-new Familiar('5821a', 3, 'p', 'c', 'Carolina', '-', 'f', '', '', false, '-'),
-new Familiar('58211', 4, 'f', 's', 'Simón', '-', 'm', '', '', false, '-'),
-new Familiar('5822', 3, 'f', 's', "David Eduardo", "Gugliesi Rodríguez", 'm', '15-mayo', 'v', false, ''-''),
+new Familiar('58', 1, 'f', 'c', 'Carmen Judith', 'Chirivella Varela', 'f', '16-julio', '29-agosto', false, '-', true),
+new Familiar('58a', 1, 'p', 'c', 'Marco Antonio', 'Rodríguez', 'm', '07-octubre', 'v', false, '-', true),
+new Familiar('581', 2, 'f', 'c', 'Ana Rita', 'Rodríguez Chirivella', 'f', '16-septiembre', 'v', true, '-', true),
+new Familiar('581a', 2, 'p', 'c', 'Abelardo Rafael', 'Valentíner Chirivella', 'm', '30-septiembre', 'v', false, '-', true),
+new Familiar('5811', 3, 'f', 'c', 'Abelardo Antonio', 'Valentíner Rodríguez', 'm', '10-noviembre', 'v', false, '-', true),
+new Familiar('5811a', 3, 'p', 'c', 'Mariangeles', 'López', 'f', '07-diciembre', 'v', false, '-', true),
+new Familiar('582', 2, 'f', 'c', 'Carmen Cecilia', 'Rodríguez Chirivella', 'f', '16-mayo', 'v', true, '-', true),
+new Familiar('582a', 2, 'p', 'c', 'Omar Sergio', 'Gugliesi', 'm', '16-marzo', 'v', false, '-', true),
+new Familiar('5821', 3, 'f', 'c', 'Sergio Andrés', 'Gugliesi Rodríguez', 'm', '01-octubre', 'v', false, '-', true),
+new Familiar('5821a', 3, 'p', 'c', 'Carolina', '-', 'f', '', '', false, '-', true),
+new Familiar('58211', 4, 'f', 's', 'Simón', '-', 'm', '', '', false, '-', true),
+new Familiar('5822', 3, 'f', 's', "David Eduardo", "Gugliesi Rodríguez", 'm', '15-mayo', 'v', false, ''-'', true),
 ]
